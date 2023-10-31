edde75df69df4caed77a6c9a49fc7ee02bf1a688
Vishvanathan K <vishvanathank@outlook.in>
2023-10-31 19:00:17 +0530

    Change website name

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,37 +1,27 @@
 import { Head } from "$fresh/runtime.ts";
 import { useSignal } from "@preact/signals";
-import { Input } from "../components/input.tsx";
-import { Button } from "../components/Button.tsx";
-import { Slider } from "../components/slider.tsx";
-
 export default function Home() {
   return (
     <>
       <Head>
-        <title>CineOphile</title>
+        <title>Pixel Perfect Pictures</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#ECEFF4]">
+      <div class="px-4 py-8 mx-auto bg-[rgb(236,239,244)]">
         <div class="flex flex-row space-x-5">
           <div class=" items-end justify-end">
             <img
               class="my-6"
               src="/logo.png"
-              width="512"
-              height="512"
+              width="256"
+              height="256"
               alt="our logo"
             />
           </div>
           <div class=" items-center justify-center text-center">
             <h1 class="text-2xl font-bold">
-              Welcome to CineOPhile!!
+              Welcome to Pixel Perfect Pictures!!
             </h1>
-            <Button>submit</Button>
-            <form>
-              <Input placeholder="Hello world"/>
-              <Slider />
-            </form>
           </div>
-
         </div>
       </div>
     </>
